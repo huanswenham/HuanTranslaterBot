4e39c5308a52aeff415e7b4fedf110e1b5244c0e
huanswenham <huanswenham@gmail.com>
2023-03-27 16:24:48 +0100

    Add deepl dependency, implement translation func

--- src/main/java/translatorBot/Bot.java
+++ src/main/java/translatorBot/Bot.java
@@ -9,36 +9,61 @@ import java.util.Properties;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import com.deepl.api.*;
+
 public class Bot extends TelegramLongPollingBot {
+  private String botUserName = "HuanTranslaterBot";
+  private Translator translator;
+
+  public Bot() {
+    initializeTranslator();
+  }
 
   @Override
   public String getBotUsername() {
-    return "HuanTranslaterBot";
+    return this.botUserName;
   }
 
   @Override
   public String getBotToken() {
+    return getBotConfigProperty("BOT_API_TOKEN");
+  }
+
+  @Override
+  public void onUpdateReceived(Update update) {
+    System.out.println(update);
+  }
+
+  private String translateText(String text, String targetLang) throws Exception {
+    TextResult result =
+        translator.translateText(text, null, targetLang);
+    String translateResult = result.getText();
+    System.out.println(translateResult);
+    return translateResult;
+  }
+
+  private void initializeTranslator() {
+    // get DEEPL API Token and initialize translator
+    String authKey = getBotConfigProperty("DEEPL_API_TOKEN");
+    translator = new Translator(authKey);
+  }
+
+  private String getBotConfigProperty(String key) {
     try (InputStream input = new FileInputStream("src/main/resources/botConfig.properties")) {
       Properties properties = new Properties();
 
       // load botConFig properties file
       properties.load(input);
 
-      String botToken = properties.getProperty("BOT_API_TOKEN");
+      String botToken = properties.getProperty(key);
       if (botToken == null) {
-        throw new NoSuchElementException("BOT_API_TOKEN field not defined in botConfig.properties.");
+        throw new NoSuchElementException(key + " field not defined in botConfig.properties.");
       }
 
-      return properties.getProperty("BOT_API_TOKEN");
-
+      return properties.getProperty(key);
     } catch (IOException ex) {
       ex.printStackTrace();
     }
     return null;
-  }
-
-  @Override
-  public void onUpdateReceived(Update update) {
-    System.out.println(update);
   }
 }
